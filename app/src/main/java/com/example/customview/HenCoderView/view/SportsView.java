475125008558484d12670aed0994cf09c502c68b
Wjy <1299454972@qq.com>
2022-04-29 23:46:05 +0800

    1.蒙版（圆形头像）
    2.环、进度条

--- app/src/main/java/com/example/customview/HenCoderView/view/SportsView.java
+++ app/src/main/java/com/example/customview/HenCoderView/view/SportsView.java
@@ -14,7 +14,10 @@ import com.example.customview.HenCoderView.Util;
 public class SportsView extends View {
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
     private static final int RADIUS = (int) Util.dp2px(150);
-    private static final int RING_WIDTH = (int) Util.dp2px(3);
+    private static final int RING_WIDTH = (int) Util.dp2px(20);
+    private static final int CIRCLE_COLOR = Color.parseColor("#90A4AE");
+    private static final int HIGHTLIGHT_COLOR = Color.parseColor("#FF4081");
+
 
     public SportsView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -26,17 +29,16 @@ public class SportsView extends View {
 
         //绘制环
         paint.setStyle(Paint.Style.STROKE);
-        paint.setColor(Color.parseColor("#2979FF"));
+        paint.setColor(CIRCLE_COLOR);
         paint.setStrokeWidth(RING_WIDTH);
         canvas.drawCircle(getWidth()/2,getHeight()/2,RADIUS,paint);
 
         //绘制进度条
-        paint.setColor(Color.parseColor("#FFFFFF"));
+        paint.setColor(HIGHTLIGHT_COLOR);
         paint.setStrokeCap(Paint.Cap.ROUND);    // 使终点是圆点
-        canvas.drawCircle(getWidth()/2,getHeight()/2,RADIUS,paint);
         canvas.drawArc(getWidth()/2-RADIUS,getHeight()/2-RADIUS,
                 getWidth()/2+RADIUS,getHeight()/2+RADIUS,
-                -90,225,
+                -90,255,
                 false,paint);
     }
 }
